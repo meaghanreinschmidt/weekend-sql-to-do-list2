9040d6bcbe83f4ecf1eeec27beb0982673bb447a
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-08-15 15:07:19 -0500

    changed port number

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // console.log('Is nodemon working?')
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // This is how our index.html appears
 app.use(express.static('server/public'));
